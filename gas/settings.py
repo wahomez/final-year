da9afa0594d56dfc6a1c0c0e2745d43fe3cb8741
Jeff Rureri <jeff.rureri@student.cuk.ac.ke>
2024-01-18 01:38:38 +0300

    Tweak

--- gas/settings.py
+++ gas/settings.py
@@ -148,6 +148,9 @@ STATICFILES_DIRS = [
 
 STORAGES = {
     # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
